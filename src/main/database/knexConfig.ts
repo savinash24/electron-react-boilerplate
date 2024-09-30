5d43e8d42f62b74abcb8733b110a51a54fcba49c
chattisanjana <avinash.nani789@gmail.com>
2024-09-30 12:03:02 +0530

    modified

--- src/main/database/knexConfig.ts
+++ src/main/database/knexConfig.ts
@@ -1,36 +1,4 @@
-// // release/app/knexConfig.ts
-// import { Knex } from 'knex';
-
-// const createKnexConfig = (dbFilePath: string): { [key: string]: Knex.Config } => {
-//   return {
-//     development: {
-//       client: 'better-sqlite3',
-//       connection: {
-//         filename: dbFilePath,  // Use the dynamic path passed as an argument
-//       },
-//       useNullAsDefault: true, // Required for SQLite
-//     },
-//     production: {
-//       client: 'better-sqlite3',
-//       connection: {
-//         filename: dbFilePath,  // Use the dynamic path passed as an argument
-//       },
-//       useNullAsDefault: true, // Required for SQLite
-//     },
-//   };
-// };
-
-// export default createKnexConfig;
-// release/app/knexConfig.ts
-// release/app/knexConfig.ts
 import { Knex } from 'knex';
-import path from 'path';
-
-// Set the base path for migrations
-
-// const migrationsDir = path.join(__dirname, 'main/database/migrations');
-// console.log({migrationsDir});
-
 const createKnexConfig = (
   dbFilePath: string,
   migrationPath: any = '',
@@ -58,5 +26,4 @@ const createKnexConfig = (
     },
   };
 };
-
 export default createKnexConfig;
